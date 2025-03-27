5efcabb2f6e29912f427f7824c66ee49eb29dd7c
mvas-procont <mihail.vasiliev@procont.ru>
2025-03-27 17:55:59 +0300

    украл у конкурентов определенгие id

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,8 @@ function App() {
       console.log("User data not available.");
     }
   }
-
+  const tgId = new URLSearchParams(window.location.search).get('user_id');
+  console.log(tgId);
   const userChatId = 93753787;
   const [isPopupOpen, setPopupOpen] = useState(false); // Состояние для попапа "Мои брони"
   const [isDeletePopupOpen, setDeletePopupOpen] = useState(false); // Состояние для попапа подтверждения удаления
